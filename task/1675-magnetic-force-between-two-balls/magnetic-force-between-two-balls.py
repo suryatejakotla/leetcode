7b9ce9ee4b4f6b4953480178981b5c03819939b6
suryatejakotla <162956165+suryatejakotla@users.noreply.github.com>
2024-06-20 07:28:51 +0530

    Time: 914 ms (50.71%) | Memory: 29.9 MB (99.15%) - LeetSync

---
+++ task/1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxDistance(self, position, m):
+        position.sort()
+        n = len(position)
+
+        def countBalls(d):
+            n_balls, cur = 1, position[0]
+            for i in range(1, n):
+                if position[i]-cur >= d:
+                    n_balls += 1
+                    cur = position[i]
+            return n_balls
+
+        left = 1; right =  position[n-1]-position[0]
+        
+        while left <= right:
+            mid = (left+right)//2
+            if countBalls(mid) >= m:
+                left = mid+1
+            else:
+                right = mid-1
+        return right
